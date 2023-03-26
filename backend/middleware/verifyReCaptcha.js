bfe16fbd40963bdc95de02de9765de6ee8638987
adrian <xwrobel02@gmail.com>
2023-03-26 23:32:14 +0200

    dependencies fix

--- backend/middleware/verifyReCaptcha.js
+++ backend/middleware/verifyReCaptcha.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const verifyReCaptcha = (req, res, next) => {
     const secretKey = process.env.SECRET_KEY
