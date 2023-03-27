faecc912b6a9aae74a707240379ef5ce9e409791
Adrian-6 <118489367+Adrian-6@users.noreply.github.com>
2023-03-27 14:03:51 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ async function main() {
 }
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Origin', ['https://url-shrtnr-app.netlify.app']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
